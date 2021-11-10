27e0efe43777c4b125794f91397a98b56db8c97e
olivier <olivier.denis@hotmail.be>
2021-11-10 13:29:09 +0100

    .

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@ import "./assets/styles/styles.scss";
 import "./index.scss";
 import "./assets/javascripts/variables";
 import "./assets/javascripts/app";
+
+import { gsap } from "gsap";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var tl = gsap.timeline();
+  tl.to(".title--mid", { opacity: 1, x: 0, duration: 1, ease: "power2" });
+  tl.to(".p-big", {
+    opacity: 1,
+    x: 0,
+    duration: 1,
+    ease: "power2",
+    delay: -0.3,
+  });
+  tl.to(".icon", {
+    opacity: 1,
+    x: 0,
+    duration: 1,
+    ease: "power2",
+    delay: -0.3,
+  });
+});
